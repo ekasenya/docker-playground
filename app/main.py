89bf031ed2f6cacf8a96b1ce3c3c80f1d5e09420
Ekaterina Senenkova <esenenkova@g,ail.com>
2023-06-10 14:13:42 +0200

    Add main

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {"message": "Hi there"}
+
+
+@app.get('/health')
+async def check_health():
+    return {"status": "OK"}
